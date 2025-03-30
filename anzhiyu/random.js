4c0fb976f494449d28732925d3d57ab8a82aaf1b
lyay23 <3050681193@qq.com>
2025-03-30 16:53:57 +0800

    Site updated: 2025-03-30 16:53:56

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/01/we/","2025/03/30/聊天记录/"];function toRandomPost(){
+var posts=["2025/03/01/we/","2025/03/30/聊天记录/","2025/03/30/圆鼻孔/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
